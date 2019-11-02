2053875c8caaad55909896eb3dca021f9f213b68
= <=>
2019-11-01 20:12:47 -0600

    refactor(*): Change to ES6

--- chores.js
+++ chores.js
@@ -1,78 +1,75 @@
-let choreList = document.getElementById('list')
-let chores = []
+const choreList = document.getElementById('list');
+let chores = [];
 
-function clearList(unorderedListElement) {
-	while(unorderedListElement.firstChild ){
-		unorderedListElement.removeChild(unorderedListElement.firstChild)
+const clearList = unOrderedListElement => {
+	while(unOrderedListElement.firstChild){
+		unOrderedListElement.removeChild(unOrderedListElement.firstChild);
 	}
-}
+};
 
-function toggleChore(nameNode, choreNode) {
-	nameNode.classList.toggle('disabled')
-	choreNode.classList.toggle('disabled')
-}
+const toggleChore = (nameNode, choreNode) => {
+	nameNode.classList.toggle('disabled');
+	choreNode.classList.toggle('disabled');
+};
 
-function writeList() {
-	clearList(choreList)
+const writeList = () => {
+	clearList(choreList);
 	
-	chores.forEach((chore, index) => {
-		let choreNode = document.createElement('li')
-		let nameNode = document.createElement('div')
-		let checkBoxNode = document.createElement('input')
-		let deleteButtonNode = document.createElement('input')
+	for(const index in chores) {
+		const chore = chores[index];
+		const choreNode = document.createElement('li');
+		const nameNode = document.createElement('div');
+		const checkBoxNode = document.createElement('input');
+		const deleteButtonNode = document.createElement('input');
 
-		checkBoxNode.setAttribute('id', `task_${index}_check`)
-		checkBoxNode.setAttribute('onclick', `checkChore(${index})`)
-		checkBoxNode.setAttribute('type', 'checkbox')
+		checkBoxNode.setAttribute('id', `task_${index}_check`);
+		checkBoxNode.setAttribute('onclick', `checkChore(${index})`);
+		checkBoxNode.setAttribute('type', 'checkbox');
 
 		if (chore.isDone) {
-			checkBoxNode.setAttribute('checked', '')
-
-			toggleChore(nameNode, choreNode)
+			checkBoxNode.setAttribute('checked', '');
+			toggleChore(nameNode, choreNode);
 		} else {
-			checkBoxNode.removeAttribute('checked')
+			checkBoxNode.removeAttribute('checked');
 		}
 		
-		choreNode.setAttribute('id', `task_${index}`)
-		deleteButtonNode.setAttribute('type', 'button')
-		deleteButtonNode.setAttribute('value', 'Delete')
-		deleteButtonNode.setAttribute('onclick', `deleteChore(${index})`)
-		nameNode.setAttribute('id', `chore_${index}_name`)
-		nameNode.appendChild(document.createTextNode(chore.name))
-		choreNode.appendChild(nameNode)
-		choreNode.appendChild(checkBoxNode)
-		choreNode.appendChild(deleteButtonNode)
-		choreList.appendChild(choreNode)
-	})
-}
-
-function checkChore(id) {
-	chore = document.getElementById(`task_${id}`)
-	let nameNode = document.getElementById(`chore_${id}_name`)
-	choreCheckbox = document.getElementById(`task_${id}_check`)
-	chores[id].isDone = choreCheckbox.checked
-	console.log(`chores[${id}].isDone`)
-	console.log(chores[id].isDone)
-	if (!chores[id].isDone) choreCheckbox.removeAttribute('checked')
-	toggleChore(chore, nameNode)
-}
-
-function addChore() {
-	let newChore = {
-		"name": "",
-		"isDone": false
+		choreNode.setAttribute('id', `task_${index}`);
+		deleteButtonNode.setAttribute('type', 'button');
+		deleteButtonNode.setAttribute('value', 'Delete');
+		deleteButtonNode.setAttribute('onclick', `deleteChore(${index})`);
+		nameNode.setAttribute('id', `chore_${index}_name`);
+		nameNode.appendChild(document.createTextNode(chore.name));
+		choreNode.appendChild(nameNode);
+		choreNode.appendChild(checkBoxNode);
+		choreNode.appendChild(deleteButtonNode);
+		choreList.appendChild(choreNode);
 	}
-	let newChoreField = document.getElementById('newChore')
+};
 
-	newChore.name = newChoreField.value
-	newChoreField.value = ''
+const checkChore = id => {
+	chore = document.getElementById(`task_${id}`);
+	const nameNode = document.getElementById(`chore_${id}_name`);
+	choreCheckbox = document.getElementById(`task_${id}_check`);
+	chores[id].isDone = choreCheckbox.checked;
+	if (!chores[id].isDone) choreCheckbox.removeAttribute('checked');
+	toggleChore(chore, nameNode);
+};
 
-	chores.push(newChore)
-	writeList()
-}
+const addChore = () => {
+	const newChore = {
+		name: '',
+		isDone: false
+	};
+	const newChoreField = document.getElementById('newChore');
 
-function deleteChore(id) {
-	chores.splice(id, 1)
-	writeList()
-}
+	newChore.name = newChoreField.value;
+	newChoreField.value = '';
 
+	chores.push(newChore);
+	writeList();
+};
+
+const deleteChore = id => {
+	chores.splice(id, 1);
+	writeList();
+};
